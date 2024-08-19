f2605514fb49762426cd7de9e5bc4f3818e16b3f
bchr1241 <bj.chavezr@alum.up.edu.pe>
2024-08-19 05:45:30 +0000

    Subiendo Problema 2(1)

--- Problema2.py
+++ Problema2.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import sqlite3
+from sqlalchemy import create_engine
+
 df_winemag = pd.read_csv("./data/winemag-data-130k-v2.csv")
 
 # Explorando el DataFrame
@@ -43,7 +46,109 @@ df_renombrado = pd.merge(df_renombrado, df_paises, how = "left", left_on= "pais"
 print(df_renombrado.head())
 print(df_renombrado.shape)
 
-#df_renombrado.to_excel('./data/winemagggggg.xlsx', index = False)
+# Generacion de reportes
+
+#Reporte 1
+
+# Mejor vino puntuado por continente
+report1 = df_renombrado.groupby('continente').apply(
+    lambda x: x.loc[x['points'].idxmax()]
+).reset_index(drop=True)
+
+print("\nVinos mejor puntuados por continente:")
+print(report1[['continente', 'title', 'points', 'price']])
+
+#Reporte 2
+
+# Promedio de precio de vino por país
+report2 = df_renombrado.groupby('pais').agg({
+    'price': 'mean',
+}).reset_index().sort_values(by='price', ascending=False)
+
+print("\nPromedio de precio de vino según país, ordenado por precio:")
+print(report2)
+
+
+#Reporte 3
+
+# Agrupar por categoría 'Tier' y calcular el conteo y precio promedio
+report3 = df_renombrado.groupby('Tier').agg({
+    'title': 'count',
+    'price': 'mean'
+}).reset_index().rename(columns={'title': 'Number_of_Wines'}).sort_values(by='Number_of_Wines', ascending=False)
+
+print("\nDistribución de vinos por categoría 'Tier':")
+print(report3)
+
+#Reporte 4
+
+# Ordenar por precio y obtener los 10 vinos más caros por continente
+top_10_expensive_wines = df_renombrado.sort_values(by=['continente', 'price'], ascending=[True, False]).groupby('continente').head(10)
+
+print("\nTop 10 vinos más caros por continente:")
+print(top_10_expensive_wines[['continente', 'title', 'price']])
+
+# Exportando reportes a la carpeta 'reportes_generados'
+
+# 1. CSV
+report1.to_csv("./reportes_generados/report1_vinos_mejor_puntuados_por_continente.csv", index=False)
+report2.to_csv("./reportes_generados/report2_promedio_precio_y_reviews_por_pais.csv", index=False)
+report3.to_csv("./reportes_generados/report3_distribucion_por_categoria_tier.csv", index=False)
+top_10_expensive_wines.to_csv("./reportes_generados/report4_top_10_vinos_mas_caros_por_continente.csv", index=False)
+
+# 2. XLSX
+report1.to_excel('./reportes_generados/report1_vinos_mejor_puntuados_por_continente.xlsx', index = False)
+report2.to_excel("./reportes_generados/report2_promedio_precio_y_reviews_por_pais.xlsx", index=False)
+report3.to_excel("./reportes_generados/report3_distribucion_por_categoria_tier.xlsx", index=False)
+top_10_expensive_wines.to_excel("./reportes_generados/report4_top_10_vinos_mas_caros_por_continente.xlsx", index=False)
+
+# 3. SQLITE
+conn = sqlite3.connect("reportes.db") #los reportes irán a reportes.db dentro del repositorio
+report1.to_sql("vinos_mejor_puntuados", conn, if_exists='replace', index=False)
+report2.to_sql("promedio_precio_por_pais", conn, if_exists='replace', index=False)
+report3.to_sql("distribucion_por_tier", conn, if_exists='replace', index=False)
+top_10_expensive_wines.to_sql("top_10_vinos_caros", conn, if_exists='replace', index=False)
+conn.close()
+
+#ENVIO MASIVO DE CORREOS
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+
+import os 
+smtp_server = 'smtp.gmail.com'  # Cambia esto al servidor SMTP que estés utilizando
+smtp_port = 587  # Cambia esto al puerto adecuado
+sender_email = 'bchr1241@gmail.com'
+sender_password = open('token.txt').read().strip() #os.environ['gmail_pass'] #
+
+# Detalles del correo electrónico
+receiver_email = 'bj.chavezr@alum.up.edu.pe'
+subject = 'Re-envio Reporte Vinos'
+body = 'Adjunto lo solicitado'
+
+msg = MIMEMultipart()
+msg['From'] = sender_email
+msg['To'] = receiver_email
+msg['Subject'] = subject
+msg.attach(MIMEText(body, 'plain'))
+
+# Adjuntar archivo
+file_path = './reportes_generados/report1_vinos_mejor_puntuados_por_continente.xlsx'  # Cambia la ruta al archivo que quieras adjuntar
+with open(file_path, 'rb') as file:
+    attachment = MIMEApplication(file.read(), _subtype="csv")
+    attachment.add_header('Content-Disposition', 'attachment', filename=file_path)
+    msg.attach(attachment)
+
+# Iniciar la conexión con el servidor SMTP
+with smtplib.SMTP(smtp_server, smtp_port) as server:
+    server.starttls()  # Iniciar el modo seguro
+    server.login(sender_email, sender_password)
+    server.sendmail(sender_email, receiver_email, msg.as_string())
+
+print('Correo enviado exitosamente')
+
 
 
 
